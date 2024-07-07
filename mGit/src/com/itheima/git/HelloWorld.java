d6161561876c51a876215230dd24683d58b27041
zh <351809836@qq.com>
2024-07-07 15:34:50 +0800

    第二次使用idea提交代码

--- mGit/src/com/itheima/git/HelloWorld.java
+++ mGit/src/com/itheima/git/HelloWorld.java
@@ -3,5 +3,6 @@ package com.itheima.git;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
     }
 }
